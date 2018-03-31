933aea541bbea78df456cc27fc13f16d579dc5f6
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-30 23:25:16 -0300

    Wired the fetchPosts Action Creator to the Posts Index component.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
 
 class PostsIndex extends Component {
+    /*
+        This function will be called by React Lifecicle function 
+        It's called when React created the component 
+        And it will be used to call our fetchPosts Action Creator
+
+            The fetchPosts Action Creator has our post content organized by key.
+
+    */
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>
@@ -12,4 +27,5 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+// here we are wiring the featchPosts Action Creator to this component (PostsIndex it self)
+export default connect( null, { fetchPosts } )(PostsIndex);
