fc98879f42db28432bdbb20e6d45485be8330d04
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-31 10:27:13 -0300

    Adding and configuring Redux Form at our ./reducer/index.js.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
+/*
+    Form Reducer: https://redux-form.com
+
+    Used to create forms and its validations.
+
+    Installation:
+        npm install --save redux-form@6.6.3
+*/
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+
+  form: formReducer
   
 });
 
