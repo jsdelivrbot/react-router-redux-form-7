6b75e8e781b313e2bf88e95c3f43fda2d65cdc67
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-31 09:55:09 -0300

    Created and added route to the component posts_new.js. Also added Switch to fix the path problem that render all components where its route path matches the URL.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 /*
-  React Router - 
+  React Router
   Instalation 
     Terminal: $ npm install --save react-router-dom@4.0.0 (or a newer version)
+
+  Switch 
+    Determines that ONLY the first path that the URL Browser matches will be rendered. 
+    Note that the path "/" was moved to end of the list.
+    It solves the render problem that renderizes all components where its paths matches 
+    the browser url. 
+      The problem without Switch: 
+        The URL browser http://localhost:8080/posts/new will render PostsIndex and PostsNew. 
+        Because the / after 8080 will match the route path="/" and the /posts/new will match the
+        router path="/posts/new". 
+        With Switch, just the first route path matched will be rendered. This is why we need to
+        change the more specific paths to be listed first. 
+
 */
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 
 
@@ -22,7 +36,10 @@ ReactDOM.render(
 
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
 
