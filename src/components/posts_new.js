63f24d48f47ab9551ac338fc7bee5bbcc8862fc6
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-31 15:48:40 -0300

    Added validation to just show the error validation message after the user have touched the input area.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -18,7 +18,7 @@ class PostsNew extends Component {
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                { field.meta.touched ? field.meta.error : '' }
             </div>
         );
     }
