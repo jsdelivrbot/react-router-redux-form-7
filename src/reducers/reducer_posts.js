751035d0dca4084f8ea8121dc20f703255aab1e5
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-30 23:00:42 -0300

    Added the Fetch Posts reducer. It will also re-structure the payload response from reduxblog/herokuapp adding the id as the key for each payload response (post list) through lodash.

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+
+import { FETCH_POSTS } from '../actions/';
+
+export default function (state, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            console.log(action.payload.data); // [post1, post2, ...]
+
+            // Adjusting the payload to load State with a better structure, 
+            // where we will have 
+            /*
+                [
+                    postId: {post content}
+                ]
+            */
+            // we will be using LODASH to do that _ (the underscore)
+            
+            return _.mapKeys(action.payload.data, 'id');
+
+        default: 
+            return state;
+    }
+
+}
