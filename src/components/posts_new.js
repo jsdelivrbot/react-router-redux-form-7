bc59fa49eb5131f6d55b62ca8c4dfba398ea4c24
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-31 11:41:27 -0300

    Adding reduxForm to the posts_new, wiring it to our reducerForm configured at ../reducers/index.js. It will allows a direct conection between our component and the Redux Store.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+/*
+    redux-form
+        reduxForm 
+            It is similar to a Connector, however it will help our component to connect directly 
+            to the Redux Store, that means, to the reducer we have configured at ../reducers/index.js.
+*/
+import { Field, reduxForm } from 'redux-form';
 
 
 class PostsNew extends Component {
     render () {
         return (
-            <div>
-                Posts New!
-            </div>
+            <form>
+                <Field
+                    name="title"
+                    // component={}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+
+/*
+    reduxForm({...}) - it wires our component to the formReducer, configured 
+    into our ../reducers/index.js
+*/
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
