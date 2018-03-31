97349abd3f4c22156a0161e93584202865d42683
rsmoreira <rsmoreira.mail@gmail.com>
2018-03-30 22:28:39 -0300

    Created the action/funcion to featch data at https://reduxblog.herokuapp.com/ API

---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
+const API_KEY = '?key=PAPERCLIP1234'
+
+/*
+    >> fetchPosts
+
+    This function will be used to Fetch the Posts at https://reduxblog.herokuapp.com/
+
+    So, how we will have network requests here, so Axios will be need to handler this requests. 
+    [https://github.com/axios/axios]
+    
+    The Redux Promise will also be needed to handle the asynchronous nature of the request it self.
+    [https://www.npmjs.com/package/redux-promise]
+
+    installation
+        terminal: $ npm install --save axios redux-promise
+
+*/
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    };
+}
+    
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+/*
+  React Router - 
+  Instalation 
+    Terminal: $ npm install --save react-router-dom@4.0.0 (or a newer version)
+*/
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
