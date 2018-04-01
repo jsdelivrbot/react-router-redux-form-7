8d9d937360252cb871bfd2bb97e8952550c0f306
rsmoreira <rsmoreira.mail@gmail.com>
2018-04-01 20:25:02 -0300

    Added back to index button navigation and added link for each particular post on the index page.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions'
+
 
 class PostsShow extends Component {
     /**
@@ -39,6 +41,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back to Index</Link>
                 <h3>{ post.title }</h3>
                 <h6>Categories: { post.categories }</h6>
                 <p>{ post.content }</p>
