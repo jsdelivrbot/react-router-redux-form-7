db46fba7f25c72fcec9a0d3c17c029d2ba80141f
rsmoreira <rsmoreira.mail@gmail.com>
2018-04-01 21:55:40 -0300

    Added reducer treatment for delete post.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case DELETE_POST:
+            // this line below removes the post id we have just deleted from the state
+            // return a new state without that id.
+            return _.omit(state, action.payload);
         case FETCH_POST:
             // const post = action.payload.data;
             // const newState = {...state};
             // newState[post.id] = post;
             // return newState;
 
-            // This line bellow does the same thing as the four above.
+            // This line below does the same thing as the four above.
             return { ...state, [action.payload.data.id] : action.payload.data }
         
         case FETCH_POSTS:
